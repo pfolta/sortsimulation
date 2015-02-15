c34952277397c07345ac7d0f75e4069065527fef
Peter Folta <mail@peterfolta.net>
2015-02-15 20:40:11 +0000

    Added Mac OS X Launcher

---
+++ run/macosx/SortSimulation.sh
@@ -0,0 +1,30 @@
+#
+# SortSimulation - A visual representation of sorting algorithms
+# Copyright (C) 2008-2014 Peter Folta. All rights reserved.
+# 
+# Project:		SortSimulation 
+# Version:		2.0.1
+# Website:		http://www.peterfolta.net/software/sortsimulation
+# 
+# File:			SortSimulation.sh
+# Created:		2015/02/15
+# Last modified:	2015/02/15
+# Author:		Peter Folta <mail@peterfolta.net>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#!/bin/sh
+
+java -XstartOnFirstThread -jar SortSimulation.jar
