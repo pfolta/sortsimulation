7e776bd8d0ec1c8cf3f23daba97e483a3cef8ca3
Peter Folta <mail@peterfolta.net>
2021-02-24 17:29:20 +0000

    Add support for styled-components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import styled from "styled-components";
+
+const Header = styled.header`
+    background-color: peachpuff;
+`;
 
 const App = (): JSX.Element => (
-    <header>
+    <Header>
         <h1>SortSimulation</h1>
-    </header>
+    </Header>
 );
 
 export default App;
