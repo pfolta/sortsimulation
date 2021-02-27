29b11853e4f6899fe462b37f7180c573b599e628
Peter Folta <mail@peterfolta.net>
2021-02-27 11:44:50 +0000

    Implement Fisher-Yates to shuffle an array in-place

--- src/app/utils/array.ts
+++ src/app/utils/array.ts
@@ -1,3 +1,4 @@
+// Determine whether an array is sorted
 const isSorted = (array: number[]): boolean => {
     for (let i = 1; i < array.length; i++) {
         if (array[i - 1] > array[i]) {
@@ -8,4 +9,24 @@ const isSorted = (array: number[]): boolean => {
     return true;
 };
 
+// Shuffle an array in-place using Fisher-Yates
+// See https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+const shuffle = (array: number[]): number[] => {
+    for (let i = array.length - 1; i > 0; i--) {
+        swap(array, i, Math.floor(Math.random() * i));
+    }
+
+    return array;
+};
+
+// Swap two elements in an array in-place
+const swap = (array: number[], i: number, j: number): number[] => {
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    return array;
+};
+
 export { isSorted };
+export { shuffle };
+export { swap };
--- test/app/utils/array.spec.ts
+++ test/app/utils/array.spec.ts
@@ -1,4 +1,4 @@
-import { isSorted } from "@/app/utils/array";
+import { isSorted, shuffle, swap } from "@/app/utils/array";
 
 describe("Array Utils", () => {
     describe("isSorted", () => {
@@ -6,7 +6,7 @@ describe("Array Utils", () => {
             expect(isSorted([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(true);
         });
 
-        it("returns true for a sorted array containing duplicates", () => {
+        it("returns true for a sorted array with duplicates", () => {
             expect(isSorted([0, 1, 2, 2, 3, 4, 4, 4])).toBe(true);
         });
 
@@ -20,6 +20,36 @@ describe("Array Utils", () => {
 
         it("returns false for an unsorted array", () => {
             expect(isSorted([3, 1, 10, 2, 6, 7, 5, 4])).toBe(false);
+        });
+    });
+
+    describe("shuffle", () => {
+        it("leaves an empty array untouched", () => {
+            expect(shuffle([])).toEqual([]);
+        });
+
+        it("leaves an array with only one element untouched", () => {
+            expect(shuffle([3])).toEqual([3]);
+        });
+
+        it("returns a shuffled array", () => {
+            expect(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).not.toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        });
+
+        it("shuffles the array in-place", () => {
+            const array = [1, 2, 3, 4, 5];
+            expect(shuffle(array)).toEqual(array);
+        });
+    });
+
+    describe("swap", () => {
+        it("swaps the correct elements in an array", () => {
+            expect(swap([1, 2, 3, 4, 5], 1, 4)).toEqual([1, 5, 3, 4, 2]);
+        });
+
+        it("swaps the elements in-place", () => {
+            const array = [1, 2, 3, 4, 5];
+            expect(swap(array, 2, 3)).toEqual(array);
         });
     });
 });
